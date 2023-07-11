261b0aac11b7728762692f93dfd01a88758e4b71
Ciprian Vlad <supercoder.cipi@gmail.com>
2023-07-11 11:27:05 +0200

    deployment

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,6 +3,7 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/auth/authSlice"
 
+
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
@@ -11,7 +12,7 @@ export const store = configureStore({
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
 
-    devTools: true
+    devTools: false
 })
 
 setupListeners(store.dispatch)
